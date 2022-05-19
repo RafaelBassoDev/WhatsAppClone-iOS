df825224a430e47c49ba5e19fb2c9b17bd29c8ff
Rafael Basso <44483784+RafaelBassoDev@users.noreply.github.com>
2022-05-19 14:56:18 -0300

    initial commit

---
+++ WhatsAppClone-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsAppClone-iOS
+//
+//  Created by Rafael Basso on 19/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WhatsAppClone-iOSUITests/WhatsAppClone_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WhatsAppClone_iOSUITestsLaunchTests.swift
+//  WhatsAppClone-iOSUITests
+//
+//  Created by Rafael Basso on 19/05/22.
+//
+
+import XCTest
+
+class WhatsAppClone_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
